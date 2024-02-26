0fa7ae1ce5a2a8778d7ee0503dba955310286e43
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-02-26 23:13:56 +0600

    fetching a single room data is successful

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,6 +37,13 @@ async function run() {
     app.get('/rooms', async(req, res)=>{
         const cursor = roomCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/rooms/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await roomCollection.findOne(query);
         res.send(result);
     })
 
